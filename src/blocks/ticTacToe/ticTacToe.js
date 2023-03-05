5a7dc77324c735ed9120b7c9ed39bc79dc14623c
Evgeny Bannikov <evgeniibannikov03@gmail.com>
2023-03-05 23:16:34 +0500

    Почти готов режим игры в крестики нолики для двоих

--- src/blocks/ticTacToe/ticTacToe.js
+++ src/blocks/ticTacToe/ticTacToe.js
@@ -13,22 +13,92 @@ twoPlayerBtn.addEventListener('click', () => {
   popupClose(twoPlayerBtn.closest('.popup'));
 });
 
-
 function rivalGame() {
   const gameArea = document.querySelector('.tic-tac-toe__container');
   const gameButtons = Array.from(gameArea.childNodes);
+  const infoArea = document.querySelector('.tic-tac-toe__game-info__container');
   let player = 'X';
 
+  const playerInfo = document.createElement('div');
+  playerInfo.className = 'game-info player-info';
+  infoArea.append(playerInfo);
+  playerInfo.textContent = 'Ход крестиков';
+
+  const gamingData = [
+    {
+      btn_11: false,
+      btn_12: false,
+      btn_13: false,
+    },
+    {
+      btn_21: false,
+      btn_22: false,
+      btn_23: false,
+    },
+    {
+      btn_31: false,
+      btn_32: false,
+      btn_33: false,
+    },
+    {
+      btn_11: false,
+      btn_21: false,
+      btn_31: false,
+    },
+    {
+      btn_12: false,
+      btn_22: false,
+      btn_32: false,
+    },
+    {
+      btn_13: false,
+      btn_23: false,
+      btn_33: false,
+    },
+    {
+      btn_11: false,
+      btn_22: false,
+      btn_33: false,
+    },
+    {
+      btn_13: false,
+      btn_22: false,
+      btn_31: false,
+    },
+  ];
+  
   gameArea.addEventListener('click', (event) => {
     if(gameButtons.includes(event.target)) {
       if(player == 'X') {
         event.target.classList.add('x-element');
         player = 'O';
-      } else {
+        playerInfo.textContent = 'Ход ноликов';
+      } else if(player == 'O') {    
         event.target.classList.add('o-element');
         player = 'X';
+        playerInfo.textContent = 'Ход крестиков';
       }
       gameButtons.splice(gameButtons.indexOf(event.target), 1);
+      
+      gamingData.forEach((obj) => {
+        let X = 0;
+        let O = 0;
+        Object.keys(obj).forEach((key) => {
+          if(event.target.classList[1] == key && obj[key] == false) {
+            obj[key] = player == 'X'?'O':'X';
+          } else if(obj[key] == 'X') {
+            X += 1;
+          } else if(obj[key]) {
+            O += 1;
+          }
+        });
+        if(X == 3) {
+          console.log('Победили X');
+        } else if(O == 3) {
+          console.log('Победили O');
+        }
+      });
+      console.log(gamingData);
     }
   });
 }
